bfb0e5600df136f74f8629954de93a30ff53a904
ErnestX <xiangspecial2012@gmail.com>
2023-03-31 11:33:13 -0400

    bug fix

--- latent_sampling.py
+++ latent_sampling.py
@@ -4,10 +4,10 @@ import numpy as np
 numOfSamples = 10
 
 #### Line sampling
-endpoint1 = np.array([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4])
-endpoint2 = np.array([5, 4, 3, 2, 1, 0, 1, 3, 2, 1])
+endpoint1 = np.array([-5, -4, -3, -2, -1, 0, 1, 2, 3])
+endpoint2 = np.array([5, 4, 3, 2, 1, 0, 1, 3, 2])
 interpolation = interp1d([0,numOfSamples-1], np.vstack([endpoint1, endpoint2]), axis=0)
-steps = np.arrange(0,numOfSamples)
+steps = np.arange(0,numOfSamples)
 samples = interpolation(steps)
 print(samples.shape)
 print(samples)
